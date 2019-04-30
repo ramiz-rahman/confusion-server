4b943b10cde32abb553e207bc2ccf00e537084e8
ramiz-rahman <ramiz.rahman@outlook.com>
2019-05-01 05:33:34 +0600

    File upload

---
+++ routes/uploadRouter.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const authenticate = require('../authenticate');
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'public/images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  }
+});
+
+const imageFileFilter = (req, file, cb) => {
+  if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+    return cb(new Error('You can upload only image files!', null));
+  }
+  cb(null, true);
+};
+
+const upload = multer({ storage: storage, fileFilter: imageFileFilter });
+
+const uploadRouter = express.Router();
+
+uploadRouter.use(bodyParser.json());
+
+uploadRouter
+  .route('/')
+  .post(
+    authenticate.verifyUser,
+    authenticate.verifyAdmin,
+    upload.single('imageFile'),
+    (req, res, next) => {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.json(req.file);
+    }
+  );
+
+module.exports = uploadRouter;
