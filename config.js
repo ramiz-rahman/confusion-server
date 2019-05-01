c79ccf3066fb5378623f8ea4225d977c5fac7a3c
ramiz-rahman <ramiz.rahman@outlook.com>
2019-05-01 23:39:27 +0600

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
   secretKey: '12345-67890-09876-54321',
-  mongoUrl: 'mongodb://localhost:27017/confusion'
+  mongoUrl: 'mongodb://localhost:27017/confusion',
+  facebook: {
+    clientId: '439290620171340',
+    clientSecret: 'af09cdd4dad91b711b63e035a5f23ed3'
+  }
 };
